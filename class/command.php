34d4b4264c8ad304ab7bcabcf0f3462da31a3ff1
Alex <alex@recordsonribs.com>
2017-10-04 16:04:49 +0100

    Add a dummy function for creating a profile

--- class/command.php
+++ class/command.php
@@ -35,6 +35,29 @@ class WPSDBCLI extends WP_CLI_Command {
 		return;
 	}
 
+	/**
+	 * Create a profile
+	 *
+	 * ## OPTIONS
+	 *
+	 * <profile>
+	 * : ID of the profile to use for the migration.
+	 *
+	 * ## EXAMPLES
+	 *
+	 * 	wp wpsdb create-profile 1
+	 *
+	 * @synopsis <profile>
+	 *
+	 * @since 1.0
+	 */
+	/**
+		* @subcommand create-profile
+		*/
+	public function create_profile( $args, $assoc_args ) {
+		WP_CLI::success( __( 'Profile created.', 'wp-sync-db-cli' ) );
+	}
+
 }
 
 WP_CLI::add_command( 'wpsdb', 'WPSDBCLI' );
