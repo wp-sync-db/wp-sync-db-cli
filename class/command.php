b96badae2bcaabe21492cbda8319d6dc180154ba
Joseph D'Souza <joseph.dsouza@alphalabs.net>
2021-06-03 13:37:18 +0100

    Add ability to manually provide connection-info and action properties rather than rely on a record being available in the database

--- class/command.php
+++ class/command.php
@@ -23,8 +23,23 @@ class WPSDBCLI extends WP_CLI_Command {
 	 */
 	public function migrate( $args, $assoc_args ) {
 		$profile = $args[0];
+		$manual_profile = [];
 
-		$result = wpsdb_migrate( $profile );
+		// Target manually (maybe no database available yet)
+		if ($assoc_args['connection-info'] && $assoc_args['action']) {
+			$manual_profile = array(
+				'connection_info' 		=> $assoc_args['connection-info'],
+				'action' 				=> $assoc_args['action'],
+				'create_backup' 		=> $assoc_args['create_backup'],
+				'backup_option' 		=> null,
+				'prefixed_tables' 		=> null,
+				'select_backup' 		=> null,
+				'table_migrate_option' 	=> null,
+				'select_tables' 		=> null,
+			);
+		}
+
+		$result = wpsdb_migrate( $profile, $manual_profile );
 
 		if ( true === $result ) {
 			WP_CLI::success( __( 'Migration successful.', 'wp-sync-db-cli' ) );
